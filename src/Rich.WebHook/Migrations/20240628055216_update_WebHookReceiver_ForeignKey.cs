1e4564dd09ae9835750f6554008fb42b8f39797a
Darren <jesn2013@hotmail.com>
2024-06-28 13:57:37 +0800

    feat: add migrations

---
+++ src/Rich.WebHook/Migrations/20240628055216_update_WebHookReceiver_ForeignKey.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rich.WebHook.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_WebHookReceiver_ForeignKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_WebHookReceivers_WebHookId",
+                table: "WebHookReceivers",
+                column: "WebHookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WebHookReceivers_WebHookSettings_WebHookId",
+                table: "WebHookReceivers",
+                column: "WebHookId",
+                principalTable: "WebHookSettings",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WebHookReceivers_WebHookSettings_WebHookId",
+                table: "WebHookReceivers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_WebHookReceivers_WebHookId",
+                table: "WebHookReceivers");
+        }
+    }
+}
